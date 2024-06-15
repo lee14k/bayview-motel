424b855a12deb0b41b0ede70cb9dbf7bd16f6855
lee14k <kmkovach@mtu.edu>
2024-06-15 11:34:05 -0400

    coming soon

---
+++ components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import { League_Spartan } from "next/font/google";
+import React from "react";
+const spartan = League_Spartan({ subsets: ["latin"] });
+
+export default function ComingSoon () {
+    return(
+    <div>
+        <h1>Coming Soon</h1>
+    </div>
+    )
+}
