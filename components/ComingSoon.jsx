8663bd5c64114518d20b341b36fff8f6efa17f37
lee14k <kmkovach@mtu.edu>
2024-06-17 13:39:44 -0400

    wrong number

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -15,10 +15,8 @@ export default function ComingSoon() {
         We are currently making some improvements to our website. Give us a call
         to book your stay!
       </p>
-      <p className="text-4xl lg:text-5xl lg:mx-48 ">
-       (906) 420-3338
-      </p>
-      <Link href="tel:+1 (906) 420-3338">
+      <p className="text-4xl lg:text-5xl lg:mx-48 ">(906) 786-2843</p>
+      <Link href="tel:+1 (906) 786-2843">
         <button className="text-4xl text-sky-900 my-2 bg-sky-300 rounded-2xl px-12 py-2 flex justify-center items-center gap-10">
           <PhoneIcon className="h-12 w-12" />
           Book your stay
